c7643cf6b31a3db6242e4c0dc95f616db285176d
Jacob A Cram <jcram@umces.edu>
2020-11-05 13:22:30 -0500

    weight variables added

--- firstEredos.R
+++ firstEredos.R
@@ -31,12 +31,20 @@ plot(bag_02)
 
 make_microbe_series <- function(n, base = rgamma(1, 5), base_weight = 1, ac_weight = 100, neighbor_weight = 100, neighbor_vec = NULL, lag = 0, sd = 0.1){
   mts = rep(NA, length.out = n)
+  if(is.null(neighbor_vec)){
+    neighbor_vec = rep(0, n)
+  }
   mts[1] = base + rnorm(1, 0, 1)
   if(mts[1] < 0){mts[1] = 0}
   for(iter in 2:n){
-    mts[iter] = (100 * mts[iter - 1] + 1 * base)/101 + rnorm(1, 0, sd)
-    if(mts[iter] < 0){mts[iter] = 0}
-    }
+    mts[iter] = (
+      # each value is the sum of several components
+      base * base_weight + #base is the starting value, but also a value towards wich things trend 
+        mts[iter - 1] * ac_weight # autocorrelation influence, the values tend to be similar to the values before and after 
+    )/(base_weight + ac_weight) + # divide by weights
+      rnorm(1, 0, sd) # finally, add some random noise on top of everything with a specified sd = standard deviation
+    if(mts[iter] < 0){mts[iter] = 0} # if the value is below zero, just make it zero
+  }
   #mts[mts < 0] <- 0
   mts
 }
